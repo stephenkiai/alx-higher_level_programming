b93a8e28324860493d315f7fc97c65f46ece469f
stephenkiai <stephenkiai23@gmail.com>
2023-08-18 19:25:10 +0300

    get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,13 @@ from sys import argv
 if __name__ == '__main__':
 
     """Establish db connection"""
-    connection = MySQLdb.connect(host='localhost', user=argv[1], port=3306, password=argv[2], database=argv[3])
+    connection = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        port=3306,
+        password=argv[2],
+        database=argv[3]
+    )
 
     """create a cursor object"""
     my_cursor = connection.cursor()
