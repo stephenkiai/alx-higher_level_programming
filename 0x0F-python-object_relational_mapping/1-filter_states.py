d2daae621ca7099539d3948460d51887d23d50e8
stephenkiai <stephenkiai23@gmail.com>
2023-08-18 20:43:30 +0300

    filter states N%

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     """Execute an sql query"""
     my_cursor.execute(
         """SELECT * FROM states WHERE
-        states.name LIKE 'N%' ORDER BY states.id ASC""")
+        states.name LIKE BINARY 'N%' ORDER BY states.id ASC""")
 
     """Fetch results"""
     my_data = my_cursor.fetchall()
