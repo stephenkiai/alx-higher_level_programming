94db31acfcb541bd86eb503b968979c277e58bb9
stephenkiai <stephenkiai23@gmail.com>
2023-08-31 21:27:38 +0300

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,23 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #script taking in a URL, sends request to the URL, and displays body size
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-url="$1"
-
-# Send the request & store response in a variable
-response=$(curl -sI "$url")
-
-# Extract content length from the response headers
-content_length=$(echo "$response" | grep -i "content-length:" | awk '{print $2}' | tr -d '\r')
-
-if [ -z "$content_length" ]; then
-    echo "Content length not found in the response headers."
-    exit 1
-fi
-
-echo "$content_length"
+curl -s "$1" | wc -c
