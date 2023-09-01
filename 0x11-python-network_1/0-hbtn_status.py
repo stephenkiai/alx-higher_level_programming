a98783a8cc3f738f24f7bc3e5e341e446bd08ab0
stephenkiai <stephenkiai23@gmail.com>
2023-09-01 22:55:53 +0300

    whats my status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
-        print(f"- type: {type(body)}")
-        print(f"- content: {body}")
-        print(f"- utf8 content: {body.decode('utf-8')}")
+        print(f"\t- type: {type(body)}")
+        print(f"\t- content: {body}")
+        print(f"\t- utf8 content: {body.decode('utf-8')}")
