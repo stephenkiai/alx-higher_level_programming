c50ad820263a67e0a8080662219d389d4941a6f6
stephenkiai <stephenkiai23@gmail.com>
2023-09-27 01:11:51 +0300

    right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,23 +4,28 @@ const { argv } = require('process');
 
 const movieId = argv[2];
 
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
 request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const movieData = JSON.parse(body);
+    
     console.log(`Characters in "${movieData.title}":`);
     
-    movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, (charError, charResponse, charBody) => {
-        if (!charError && charResponse.statusCode === 200) {
-          const characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        } else {
-          console.error(`Error fetching character: ${charError || charResponse.statusCode}`);
-        }
+    // Iterate through characters in the same order as the list in the /films/ response
+    movieData.characters.reduce((promise, characterUrl) => {
+      return promise.then(() => {
+        return new Promise((resolve) => {
+          request(characterUrl, (charError, charResponse, charBody) => {
+            if (!charError && charResponse.statusCode === 200) {
+              const characterData = JSON.parse(charBody);
+              console.log(characterData.name);
+            }
+            resolve();
+          });
+        });
       });
-    });
+    }, Promise.resolve());
   } else {
     console.error(`Error fetching movie: ${error || response.statusCode}`);
   }
