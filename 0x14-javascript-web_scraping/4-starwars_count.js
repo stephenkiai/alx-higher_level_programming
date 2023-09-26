3340db0b00a42d12264cbf51cc0766949bfe752a
stephenkiai <stephenkiai23@gmail.com>
2023-09-27 00:41:16 +0300

    star wars wedge

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
-    const filmsData = JSON.parse(body).results;
-    const wedgeAntillesFilms = filmsData.filter(film =>
-      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
-    );
-    console.log(wedgeAntillesFilms.length);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
